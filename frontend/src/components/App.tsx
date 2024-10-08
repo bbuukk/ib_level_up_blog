1f2fe28c425a8ec532446bf789b6156540df4268
francisco <fcollazos@internetbrands.com>
2024-10-08 03:13:12 +0200

    feat(FE): install react router and react query
    * add basic react query and router setup
    * add navigation bar
    * add page with RQ basic example

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,39 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import BasicReactQuery from 'routes/BasicReactQuery';
+import Root from 'routes/Root';
+import Layout from './Layout';
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Root />,
+        errorElement: <div>Not found</div>
+      },
+      {
+        path: '/basic-react-query',
+        element: <BasicReactQuery />
+      }
+    ]
+  }
+]);
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
 }
 
 export default App;
