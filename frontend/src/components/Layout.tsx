71a95588324e548cda50dd11a5e64de8b69a11b7
francisco <fcollazos@internetbrands.com>
2024-10-24 21:26:27 +0200

    feat: integrate style files and markup authored by Marko
    * add homepage markup
    * add images
    * add global style files, accesible through index.scss
    * turn off tailwind plugin rule, so custom class names can be used
    * add sass dependency
    * update header in Layout file

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,26 +2,40 @@ import { Link, Outlet } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4">
-        <li>
-          <Link
-            to="/"
-            className="text-white transition duration-300 hover:text-gray-300"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/basic-react-query"
-            className="text-white transition duration-300 hover:text-gray-300"
-          >
-            Basics RQ
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <div className="container--header">
+        <a href="/" className="header__logo">
+          Acad asdfasdemy<span>Blog</span>
+        </a>
+        {/* add ".nav--active" modifier to trigger mobile nav */}
+        <nav className="nav">
+          <ul className="nav__list">
+            <li>
+              <Link to="/" className="nav__link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/basic-react-query" className="nav__link">
+                Basics RQ
+              </Link>
+            </li>
+          </ul>
+          <div className="nav__toggle nav__toggle--active">
+            <img
+              className="nav__open"
+              src="/src/assets/images/menu-icon.svg"
+              alt="Menu icon"
+            />
+            <img
+              className="nav__close"
+              src="/src/assets/images/close-icon.svg"
+              alt="Menu icon"
+            />
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 };
 
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,5 +1,310 @@
+import './Root.scss';
+
 const Root = () => {
-  return <h1 className="text-center text-4xl font-bold text-gray-800">Home</h1>;
+  return (
+    <>
+      <main>
+        <section className="hero">
+          <article className="container--hero">
+            <h1>
+              Lorem ipsum dolor <span>sit amet</span>
+            </h1>
+            <div className="divider" />
+            <p>
+              Donec non massa auctor, dictum ante eu, ultrices eros. Sed sit
+              amet augue nec diam tempor placerat. Integer dignissim lacinia
+              turpis.
+            </p>
+          </article>
+        </section>
+        <section className="featuredNews">
+          <div className="container--featuredNews">
+            <div className="featuredNews__heading">
+              <h2>Featured news</h2>
+              <a href="#">All featured news</a>
+            </div>
+            <div className="featuredCardsList">
+              {/* start featuredCard */}
+              <a href="" className="featuredCard">
+                <img
+                  className="featuredCard__image"
+                  src="https://picsum.photos/500/380"
+                />
+                <div className="featuredCard__overlay" />
+                <div className="featuredCard__content">
+                  <div className="premiumFlag--featuredCard">
+                    <img
+                      className=""
+                      src="/src/assets/images/premium-icon.svg"
+                    />
+                  </div>
+                  <div className="tag--featuredCard">Design</div>
+                  {/* Use this as component in featuredArticleCard and articleCard - name - "cardContent"  */}
+                  <div className="cardContent--featuredCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              {/* end featuredCard */}
+              {/* start featuredCard */}
+              <a href="" className="featuredCard">
+                <img
+                  className="featuredCard__image"
+                  src="https://picsum.photos/500/381"
+                />
+                <div className="featuredCard__overlay" />
+                <div className="featuredCard__content">
+                  <div className="premiumFlag--featuredCard">
+                    <img
+                      className=""
+                      src="/src/assets/images/premium-icon.svg"
+                    />
+                  </div>
+                  <div className="tag--featuredCard">Design</div>
+                  {/* Use this as component in featuredArticleCard and articleCard - name - "cardContent"  */}
+                  <div className="cardContent--featuredCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              {/* end featuredCard */}
+              {/* start featuredCard */}
+              <a href="" className="featuredCard">
+                <img
+                  className="featuredCard__image"
+                  src="https://picsum.photos/500/382"
+                />
+                <div className="featuredCard__overlay" />
+                <div className="featuredCard__content">
+                  <div className="premiumFlag--featuredCard">
+                    <img
+                      className=""
+                      src="/src/assets/images/premium-icon.svg"
+                    />
+                  </div>
+                  <div className="tag--featuredCard">Design</div>
+                  {/* Use this as component in featuredArticleCard and articleCard - name - "cardContent"  */}
+                  <div className="cardContent--featuredCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              {/* end featuredCard */}
+            </div>
+          </div>
+        </section>
+        <section className="latestNews">
+          <div className="container--latestNews">
+            <div className="latestNews__header">
+              <div className="latestNews__heading">
+                <h3>Latest news</h3>
+                <div className="divider--latestNews" />
+                <p>
+                  Donec non massa auctor, dictum ante eu, ultrices eros. Sed sit
+                  amet augue nec diam tempor placerat.
+                </p>
+              </div>
+              <nav>
+                <ul className="listTabs">
+                  <li className="tag--tabActive">All</li>
+                  <li className="tag--tab">AI</li>
+                  <li className="tag--tab">Design</li>
+                  <li className="tag--tab">Programming</li>
+                </ul>
+              </nav>
+            </div>
+            <div className="articlesList">
+              {/* start articleCard */}
+              <a href="" className="articleCard">
+                <div className="premiumFlag--articleCard">
+                  <img className="" src="/src/assets/images/premium-icon.svg" />
+                </div>
+                <div className="articleCard__imgBox">
+                  <img src="https://picsum.photos/500/380" alt="" />
+                </div>
+                <div className="articleCard__body">
+                  <div className="tag--articleCard">Design</div>
+                  <div className="cardContent cardContent--articleCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              {/* end articleCard */}
+              <a href="" className="articleCard">
+                <div className="premiumFlag--articleCard">
+                  <img className="" src="/src/assets/images/premium-icon.svg" />
+                </div>
+                <div className="articleCard__imgBox">
+                  <img src="https://picsum.photos/500/380" alt="" />
+                </div>
+                <div className="articleCard__body">
+                  <div className="tag--articleCard">Design</div>
+                  <div className="cardContent cardContent--articleCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              <a href="" className="articleCard">
+                <div className="premiumFlag--articleCard">
+                  <img className="" src="/src/assets/images/premium-icon.svg" />
+                </div>
+                <div className="articleCard__imgBox">
+                  <img src="https://picsum.photos/500/380" alt="" />
+                </div>
+                <div className="articleCard__body">
+                  <div className="tag--articleCard">Design</div>
+                  <div className="cardContent cardContent--articleCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+              <a href="" className="articleCard">
+                <div className="premiumFlag--articleCard">
+                  <img className="" src="/src/assets/images/premium-icon.svg" />
+                </div>
+                <div className="articleCard__imgBox">
+                  <img src="https://picsum.photos/500/380" alt="" />
+                </div>
+                <div className="articleCard__body">
+                  <div className="tag--articleCard">Design</div>
+                  <div className="cardContent cardContent--articleCard">
+                    <p className="cardContent__date">May, 4 2024</p>
+                    <h3 className="cardContent__title">
+                      Lorem ipsum dolor sit
+                    </h3>
+                    <p className="cardContent__author">By: Author Name</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="latestNews__link">
+              <a href="" className="button--latestNews">
+                News page
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <section className="premiumBanner">
+        <div className="container--premiumBanner">
+          <h3 className="premiumBanner__title">
+            Start <span>premium</span> membership
+          </h3>
+          <p className="premiumBanner__text">
+            Quisque a tincidunt tellus, sed auctor odio. Nullam fringilla purus
+            quis augue tincidunt ultrices. Nunc vitae malesuada nisi, nec
+            eleifend ante.
+          </p>
+          <a href="" className="button--premiumBanner">
+            Start premium
+          </a>
+        </div>
+      </section>
+      <footer className="footer">
+        <article className="container--footer">
+          <h3>Follow us on social media</h3>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="#">
+                <img
+                  src="/src/assets/images/facebook-logo.svg"
+                  alt="Facebook logo"
+                />
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#">
+                <img
+                  src="/src/assets/images/twitter-logo.svg"
+                  alt="Twitter logo"
+                />
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#">
+                <img
+                  src="/src/assets/images/pinterest-logo.svg"
+                  alt="Pinterest logo"
+                />
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#">
+                <img
+                  src="/src/assets/images/behance-logo.svg"
+                  alt="Behance logo"
+                />
+              </a>
+            </li>
+          </ul>
+        </article>
+      </footer>
+      <div className="modal">
+        <div className="modal__container">
+          <div className="modal__heading">
+            <h4>Modal text</h4>
+            <div className="modal__close">
+              <img
+                src="/src/assets/images/close-modal-icon.svg"
+                alt="Menu icon"
+              />
+            </div>
+          </div>
+          <div className="modal__content">
+            <form action="" className="form--simple">
+              <div className="inputBox">
+                <label htmlFor="">Label</label>
+                <input type="text" placeholder="Placeholder" />
+                <p>Error</p>
+              </div>
+              <div className="inputBox">
+                <label htmlFor="">Label</label>
+                <input type="text" placeholder="Placeholder" />
+                <p>Error</p>
+              </div>
+              <div className="inputBox">
+                <label htmlFor="">Label</label>
+                <input type="text" placeholder="Placeholder" />
+                <p>Error</p>
+              </div>
+              <div className="inputBox">
+                <label htmlFor="">Label</label>
+                <input type="text" placeholder="Placeholder" />
+                <p>Error</p>
+              </div>
+              <div className="form__buttonBox">
+                <button className="button">Action</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Root;
