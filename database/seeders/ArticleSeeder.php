2216830c9771a1859d6776664c0d9610d56592d1
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2024-11-28 00:56:33 +0200

    feat(seeding): improve seeding

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\Comment;
-use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
@@ -17,34 +16,21 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        $tags = Tag::factory()
-            ->count(2)
-            ->state(new Sequence(
-                ['label' => 'featured'],
-                ['label' => 'ai'],
-            ))
-            ->create();
+        $users = [
+            ['email' => 'user1@example.com', 'password' => 'password1'],
+            ['email' => 'user2@example.com', 'password' => 'password2'],
+            ['email' => 'user3@example.com', 'password' => 'password3'],
+        ];
 
-        //TODO: fix, seeder is very bloated
         User::factory()
             ->count(3)
-            ->state(new Sequence(
-                ['email' => 'user1@example.com', 'password' => bcrypt('password1')],
-                ['email' => 'user2@example.com', 'password' => bcrypt('password2')],
-                ['email' => 'user3@example.com', 'password' => bcrypt('password3')]
-            ))
+            ->state(new Sequence($users))
             ->has(
                 Article::factory()
                     ->count(3)
                     ->has(
                         Comment::factory(3)
                     )
-                    ->afterCreating(function (Article $article) use ($tags) {
-                        // Attach random tags to each article
-                        $article->tags()->attach(
-                            $tags->random(rand(1, 2))->pluck('id')->toArray()
-                        );
-                    })
             )
             ->create();
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
 use App\Models\Article;
 use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +18,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(2)->create();
+        $this->cleanupOldImages();
 
+        User::factory(4)->create();
+
+        $this->call(ArticleSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(TagSeeder::class);
+
+        $tags = Tag::factory()
+            ->count(2)
+            ->state(new Sequence(
+                ['label' => 'featured'],
+                ['label' => 'ai'],
+            ))
+            ->create();
+
+        //TODO: refactor
         User::factory()
             ->set('name', 'Francisco')
             ->set('email', 'francisco@internetbrands.com')
             ->set('password', 'test1234')
             ->has(
-                Article::factory(1)
+                Article::factory(8)
+                    ->afterCreating(function (Article $article) use ($tags) {
+                        $article->tags()->attach(
+                            $tags->random(rand(1, 2))->pluck('id')->toArray()
+                        );
+                    })
                     ->has(Comment::factory(1))
             )
             ->createOne();
+    }
 
-        $this->call(ArticleSeeder::class);
+    private function cleanupOldImages()
+    {
+        Storage::disk('public')->deleteDirectory('covers');
+        Storage::disk('public')->makeDirectory('covers');
     }
 }
