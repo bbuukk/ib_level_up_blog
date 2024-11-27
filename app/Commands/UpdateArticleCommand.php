896bbd626f2757421cf6a8722d4d77b6f4b182e5
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2024-11-28 01:38:35 +0200

    refactor(article-versions): migrate to capped versions collection
    - Optimize storage and query performance for article versions

--- app/Commands/UpdateArticleCommand.php
+++ app/Commands/UpdateArticleCommand.php
@@ -2,9 +2,15 @@
 
 namespace App\Commands;
 
+use Illuminate\Support\Facades\DB;
+use App\Traits\ManipulateFilesInPublicStorage;
 
 class UpdateArticleCommand
 {
+    use ManipulateFilesInPublicStorage;
+
+    private const MAX_VERSIONS = 0;
+
     public function __construct(
         private $article,
         private ?string $title,
@@ -14,19 +20,43 @@ class UpdateArticleCommand
 
     public function execute()
     {
-        $this->article->versions()->create([
-            'title' => $this->article->title,
-            'content' => $this->article->content,
-            'cover_url' => $this->article->cover_url
-        ]);
+        DB::transaction(function () {
 
-        $this->article->fill(array_filter([
-            'title' => $this->title,
-            'content' => $this->content,
-            'cover_url' => $this->coverUrl
-        ]));
+            //make versions storage capped for preventing excessive disk space usage
+            $versionsCount = $this->article->versions()->count();
+            if ($versionsCount > self::MAX_VERSIONS) {
+                $oldestVersionsToDelete = $versionsCount - self::MAX_VERSIONS;
+
+                $oldestVersions = $this->article->versions()
+                    ->oldest()
+                    ->limit($oldestVersionsToDelete)
+                    ->get();
+
+                foreach ($oldestVersions as $version) {
+                    $coverUrl = $version->cover_url;
+
+                    if ($coverUrl) {
+                        $this->deleteFileFromPublicStorage($coverUrl);
+                    }
+
+                    $version->delete();
+                }
+            }
+
+            $this->article->versions()->create([
+                'title' => $this->article->title,
+                'content' => $this->article->content,
+                'cover_url' => $this->article->cover_url
+            ]);
+
+            $this->article->fill(array_filter([
+                'title' => $this->title,
+                'content' => $this->content,
+                'cover_url' => $this->coverUrl
+            ]));
 
 
-        $this->article->save();
+            $this->article->save();
+        });
     }
 }
