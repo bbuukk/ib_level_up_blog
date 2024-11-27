80622ee5853337553c4aebc37fdb65c1e4b7dcac
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2024-11-28 01:01:29 +0200

    refactor(article-versions): migrate to capped versions collection
    - Optimize storage and query performance for article versions

--- app/Models/ArticleVersion.php
+++ app/Models/ArticleVersion.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class ArticleVersion extends Model
 {
     protected $fillable = ['article_id', 'title', 'content', 'cover_url'];
+
 
     public function article()
     {
