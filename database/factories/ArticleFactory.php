5f4b5ec586771688bce7bd1b88138ee251c9093a
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2024-11-28 00:57:54 +0200

    feat(article-factory): introduce unique fake cover image generation

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,12 +4,16 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
  */
 class ArticleFactory extends Factory
 {
+
     /**
      * Define the model's default state.
      *
@@ -19,12 +23,47 @@ class ArticleFactory extends Factory
     {
         $createdAt = $this->faker->dateTimeBetween('-1 year', 'now');
 
-        return [
+        $attributes = [
             'title' => $this->faker->sentence(6),
             'content' => $this->faker->text(1500),
             'author_id' => User::factory(),
             'created_at' => $createdAt,
             'updated_at' => $createdAt
         ];
+
+        if (Config::get('app.env', false) === 'local') {
+            $attributes['cover_url'] = $this->generateFakeImage();
+        }
+
+        return $attributes;
+    }
+
+    private function generateFakeImage()
+    {
+        $width = 800;
+        $height = 600;
+        $imageUrl = "https://picsum.photos/{$width}/{$height}";
+
+        $response = Http::get($imageUrl);
+
+        if ($response->successful()) {
+            $disk = "public";
+            $extension = 'jpg';
+            $filename = uniqid() . '.' . $extension;
+
+            // Save the image content to a temporary file
+            $tempFile = tempnam(sys_get_temp_dir(), 'img');
+            file_put_contents($tempFile, $response->body());
+
+            // Use putFileAs with the temporary file
+            $filePath = Storage::disk($disk)->putFileAs('covers/', $tempFile, $filename);
+
+            // Remove the temporary file
+            unlink($tempFile);
+
+            return Storage::disk($disk)->url($filePath);
+        }
+
+        return null;
     }
 }
